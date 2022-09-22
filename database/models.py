fe4f1bcd44eeb1ab34ce2c941a021742806946f8
tlyngen <tlyngen@gmail.com>
2022-09-21 22:01:46 -0700

    created orm added models added temporary test method to expensieve class removed old env requirements

---
+++ database/models.py
@@ -0,0 +1,62 @@
+from ast import For
+from sqlalchemy.orm import declarative_base, relationship
+from sqlalchemy import (Column, Integer, Float, String, Boolean, ForeignKey)
+from sqlalchemy.dialects.sqlite import DATETIME
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+
+    expense = relationship("Expense")
+    user_expense = relationship("UserExpense")
+
+    def __repr__(self):
+        return f"{self.id}, {self.username}, {self.password}"
+
+
+class Expense(Base):
+    __tablename__ = 'expense'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    amount = Column(Float)
+    split_type = Column(String)
+    open = Column(Boolean)
+    date = Column(DATETIME)
+
+    user = relationship("User")
+
+    def __repr__(self):
+        return f"""
+            {self.id},
+            {self.user_id},
+            {self.amount},
+            {self.split_type},
+            {self.open},
+            {self.date}"""
+
+
+class UserExpense(Base):
+    __tablename__ = 'user_expense'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("user.id"))
+    expense_id = Column(Integer, ForeignKey("expense.id"))
+    amount = Column(Float)
+    amount_paid = Column(Integer)
+
+    user = relationship("User")
+    expense = relationship("Expense")
+
+    def __repr__(self):
+        return f"""
+            {self.user_id},
+            {self.user_id},
+            {self.expense_id},
+            {self.amount},
+            {self.amount_paid},
+            {self.active}"""
---
+++ database/session.py
@@ -0,0 +1,19 @@
+import logging
+
+from sqlalchemy.orm import sessionmaker
+
+
+class Session:
+    def __init__(self, engine):
+        self.logger = logging.getLogger(__name__)
+        self.logger.info("creating session")
+        Session = sessionmaker()
+        Session.configure(bind=engine)
+        self.session = Session()
+
+    def __enter__(self):
+        return self.session
+
+    def __exit__(self, type, value, traceback):
+        self.session.commit()
+        self.session.close()
