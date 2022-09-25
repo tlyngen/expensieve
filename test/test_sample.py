597b8ac61b176b6cd0e0cc1d04936b03fb292c70
tlyngen <tlyngen@gmail.com>
2022-09-24 17:20:35 -0700

    fixed linting errors in sample test

--- test/test_sample.py
+++ test/test_sample.py
@@ -1,6 +1,7 @@
 import pytest
 
+
 def test_method1():
-	x=5
-	y=6
-	assert x+1 == y,"test failed because x=" + str(x) + " y=" + str(y)
+    x = 5
+    y = 6
+    assert x + 1 == y, "test failed because x=" + str(x) + " y=" + str(y)
